95db6e07957949ce4287e2bb565e9b2823e1ffee
bambangharyopba <bambanganggit@gmail.com>
2021-03-20 19:09:43 +0700

    add phong shading (need fix)

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import {
   getRotationMatrix,
   getPerspectiveMatrix,
   getInverse,
+  getTranspose,
   getIdentityMatrix,
   getLookAt,
 } from "./utils/Matrix4";
@@ -29,7 +30,7 @@ let vbo: WebGLBuffer | null = null;
 let wireVbo: WebGLBuffer | null = null;
 let elementVbo: WebGLBuffer | null = null;
 
-let colorOffset: number = 0;
+let normalOffset: number = 0;
 let numElements: number = 0;
 let wireNumElements: number = 0;
 
@@ -37,6 +38,17 @@ let matrixLocation: WebGLUniformLocation | null = null;
 let wireMatrixLocation: WebGLUniformLocation | null = null;
 let projectionMatrixLocation: WebGLUniformLocation | null = null;
 let wireProjectionMatrixLocation: WebGLUniformLocation | null = null;
+let normalMatrixLocation: WebGLUniformLocation | null = null;
+
+let mode: WebGLUniformLocation | null = null;
+let ka: WebGLUniformLocation | null = null;
+let kd: WebGLUniformLocation | null = null;
+let ks: WebGLUniformLocation | null = null;
+let shineVal: WebGLUniformLocation | null = null;
+let ac: WebGLUniformLocation | null = null;
+let dc: WebGLUniformLocation | null = null;
+let sc: WebGLUniformLocation | null = null;
+let lightPos: WebGLUniformLocation | null = null;
 
 let wireIndices = null;
 let matrix = Array(16).fill(0);
@@ -47,6 +59,9 @@ let yRotationCamera = 0;
 let cameraDistance = 2;
 let cameraMatrix = Array(16).fill(0);
 let projectionMatrix = Array(16).fill(0);
+
+// Normal
+let cubeNormal = new Float32Array(cubePositions);
 
 // Transformation variables
 let xRotation = 0;
@@ -103,12 +118,19 @@ function initModel() {
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
   gl.bufferData(
     gl.ARRAY_BUFFER,
-    tetrahedronPositions.byteLength + tetrahedronColors.byteLength,
+    tetrahedronPositions.byteLength + cubeNormal.byteLength,
     gl.STATIC_DRAW
   );
-  colorOffset = tetrahedronPositions.byteLength;
+  // colorOffset = tetrahedronPositions.byteLength;
+  // gl.bufferSubData(gl.ARRAY_BUFFER, 0, tetrahedronPositions);
+  // gl.bufferSubData(gl.ARRAY_BUFFER, colorOffset, tetrahedronColors);
+  //   cubePositions.byteLength + cubeNormal.byteLength,
+  //   gl.STATIC_DRAW
+  // );
+
+  normalOffset = tetrahedronPositions.byteLength;
   gl.bufferSubData(gl.ARRAY_BUFFER, 0, tetrahedronPositions);
-  gl.bufferSubData(gl.ARRAY_BUFFER, colorOffset, tetrahedronColors);
+  gl.bufferSubData(gl.ARRAY_BUFFER, normalOffset, cubeNormal);
 
   // Store element triangle definition
   elementVbo = gl.createBuffer();
@@ -147,13 +169,32 @@ function initShaders() {
 
   // Link shader variables
   gl.bindAttribLocation(programObject, 0, "a_position");
-  gl.bindAttribLocation(programObject, 1, "a_color");
+  gl.bindAttribLocation(programObject, 1, "normal");
+
+  console.log(vertexShader);
+  console.log(fragmentShader);
+
   gl.linkProgram(programObject);
+  // normalLocation = gl.getAttribLocation(programObject, "normal");
   matrixLocation = gl.getUniformLocation(programObject, "u_matrix");
   projectionMatrixLocation = gl.getUniformLocation(
     programObject,
     "u_proj_matrix"
   );
+  mode = gl.getUniformLocation(programObject, "mode");
+  ka = gl.getUniformLocation(programObject, "Ka");
+  kd = gl.getUniformLocation(programObject, "Kd");
+  ks = gl.getUniformLocation(programObject, "Ks");
+  shineVal = gl.getUniformLocation(programObject, "shininessVal");
+  ac = gl.getUniformLocation(programObject, "ambientColor");
+  dc = gl.getUniformLocation(programObject, "diffuseColor");
+  sc = gl.getUniformLocation(programObject, "specularColor");
+  lightPos = gl.getUniformLocation(programObject, "lightPos");
+  normalMatrixLocation = gl.getUniformLocation(
+    programObject,
+    "normalMat"
+  );
+
 }
 
 /**
@@ -230,14 +271,51 @@ function calculateCameraProjection() {
 }
 
 /**
+ * Function to calculate normals.
+ */
+function calculateSurfaceNormal() {
+  var vertNormal = Array(cubePositions.length).fill(0);
+  for(var i = 0; i < cubeIndices.length/3; i = i+3){
+    var p1 = [];
+    var p2 = [];
+    var p3 = [];
+    for(var j = 0; j < 3; j++){
+      p1.push(cubePositions[cubeIndices[i] * 3 + j]);
+      p2.push(cubePositions[cubeIndices[i + 1] * 3 + j]);
+      p3.push(cubePositions[cubeIndices[i + 2] * 3 + j]);
+    }
+    var u = [];
+    var v = [];
+    for(var j = 0; j < 3; j++){
+      u.push(p2[j] - p1[j]);
+      v.push(p3[j] - p1[j]);
+    }
+    var nx = u[1] * v[2] - u[2] * v[1];
+    var ny = u[2] * v[0] - u[0] * v[2];
+    var nz = u[0] * v[1] - u[1] * v[0];
+
+    var normal = [nx, ny, nz];
+
+    for(var j = 0; j < 3; j++){
+      vertNormal[cubeIndices[i] * 3 + j] += normal[j];
+      vertNormal[cubeIndices[i + 1] * 3 + j] += normal[j];
+      vertNormal[cubeIndices[i + 2] * 3 + j] += normal[j];
+    }
+  }
+  cubeNormal = new Float32Array(vertNormal);
+}
+
+/**
  * Draw objects
  */
 function draw() {
+  calculateSurfaceNormal();
+
   gl = gl as WebGLRenderingContext;
 
   // Reset canvas
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.enable(gl.CULL_FACE);
+  // gl.enable(gl.CULL_FACE);
   gl.enable(gl.DEPTH_TEST);
 
   // Use WebGL Program
@@ -247,19 +325,31 @@ function draw() {
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
   gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(0);
-  gl.vertexAttribPointer(1, 4, gl.FLOAT, false, 0, colorOffset);
+  gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, normalOffset);
   gl.enableVertexAttribArray(1);
 
   // Initiate transformation matrix
   gl.uniformMatrix4fv(matrixLocation, false, matrix);
   gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix);
+  gl.uniformMatrix4fv(normalMatrixLocation, false, new Float32Array(getTranspose(getInverse(matrix))));
+
+  // Phong shader uniform
+  gl.uniform1i(mode, 1);
+  gl.uniform1f(ka, 1);
+  gl.uniform1f(kd, 1);
+  gl.uniform1f(ks, 1);
+  gl.uniform1f(shineVal, 80);
+  gl.uniform3fv(ac, new Float32Array([0.2, 0.0, 0.0]));
+  gl.uniform3fv(dc, new Float32Array([0.7, 0.7, 0.0]));
+  gl.uniform3fv(sc, new Float32Array([1.0, 1.0, 1.0]));
+  gl.uniform3fv(lightPos, new Float32Array([0, 0, 2]));
 
   // Bind and draw triangles
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementVbo);
   gl.drawElements(gl.TRIANGLES, numElements, gl.UNSIGNED_SHORT, 0);
 
   // Draw wireframe
-  drawWire();
+  // drawWire();
 }
 
 /**
--- src/shaders/BodyVertexShader.glsl
+++ src/shaders/BodyVertexShader.glsl
@@ -1,12 +1,16 @@
-attribute vec4 a_position;
-attribute vec4 a_color;
+attribute vec3 a_position;
+attribute vec3 normal;
 
 uniform mat4 u_matrix;
 uniform mat4 u_proj_matrix;
+uniform mat4 normalMat;
 
-varying vec4 v_color;
+varying vec3 normalInterp;
+varying vec3 vertPos;
 
 void main() {
-  gl_Position = u_proj_matrix * u_matrix * a_position;
-  v_color = a_color;
+  vec4 vertPos4 = u_matrix * vec4(a_position, 1.0);
+  vertPos = vec3(vertPos4) / vertPos4.w;
+  normalInterp = vec3(normalMat * vec4(normalize(normal), 0.0));
+  gl_Position = u_proj_matrix * vertPos4;
 }
